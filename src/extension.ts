114547d9778707b3ece2ba9f39a7193b498d1a36
Ethan Strominger <ethanstrominger2@gmail.com>
2023-06-17 00:00:56 -0400

    Merge package.json and extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,31 @@ try {
 import * as vscode from "vscode";
 import { EXTENSION_CONSTANT } from "constant";
 import { LeftPanelWebview } from "providers/left-webview-provider";
-
+import { VscodeMobTimer } from "vscode-mobtimer";
 export function activate(context: vscode.ExtensionContext) {
+  const debug = context.extensionMode === vscode.ExtensionMode.Development;
+  if (debug) {
+		process.env.REACT_APP_WEBSOCKET_URL = `ws://localhost:${
+			process.env.REACT_APP_WEBSOCKET_PORT || "4000"
+		}`;
+  }
+  console.log(
+		"In extension.ts, process.env.REACT_APP_WEBSOCKET_URL = ",
+		process.env.REACT_APP_WEBSOCKET_URL
+  );
+  console.log('"mobtimer.display" is now active!');
+  let vscodeMobTimer = new VscodeMobTimer();
+  console.log("Done");
+
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("mobtimer.display", () => {
+		vscodeMobTimer.update();
+  });
+
+  context.subscriptions.push(vscodeMobTimer);
+  context.subscriptions.push(disposable);
 	let helloWorldCommand = vscode.commands.registerCommand(
-		"vscode-webview-extension-with-react.helloWorld",
+		"mobtimer.helloWorld",
 		() => {
 			vscode.window.showInformationMessage(
 				"Hello World from vscode-webview-extension-with-react!"
